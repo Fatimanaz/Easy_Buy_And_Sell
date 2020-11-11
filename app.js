080c2920fa0aeac29cef5257d0f6d46a6f71d0e3
ishank <ishank2208@gmail.com>
2020-11-11 12:43:34 +0000

    wologin

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ function escapeRegex(text) {
 
 
 //SERVER
-app.listen(3000 || process.env.port,function(req,res){
+app.listen(8000 || process.env.port,function(req,res){
 	console.log("Server Has been Started");
 });
 
